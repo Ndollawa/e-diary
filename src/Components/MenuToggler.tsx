76a88895e5e80125bcf088fae415c5233605b7de
Ndollawa <ndollawa@yahoo.com>
2022-09-12 00:03:25 +0100

    Created dropdowns

--- src/Components/MenuToggler.tsx
+++ src/Components/MenuToggler.tsx
@@ -1,5 +1,5 @@
 // import React, {useState} from 'react'
-
+import {IoMdMail,IoMdNotifications} from "react-icons/io"
 
 
 interface FuncProp {
@@ -10,12 +10,36 @@ interface FuncProp {
     // const [txt,setTxt] = useState("")
 
   return (
-    <div className="flex justify-between items-center bg-black text-white h-24">
+    <div className="flex justify-between items-center bg-black text-white h-16  z-10">
                 <h1 className="text-2xl font-bold px-4">Better Code</h1>
                 <ul className='flex w-auto h-6 justify-between'>
-                  <li className="nav-item">Link</li>
-                  <li className="nav-item">Link</li>
-                  <li className="nav-item"><div className=""><img className="" src="" alt=""/></div>
+                  <li className="mr-4 text-xl relative"><IoMdMail/>
+                  <div className="dropdown">
+                    <div className="dropdown-menu-header messages">Messages</div>
+                      <ul className="dropdown-menu">
+                        <li className="items"></li>
+                        <li className="items"></li>
+                        <li className="items"></li>
+                        <li className="items"></li>
+                        <li className="items"></li>
+                      </ul>
+                      <div className="dropdown-menu-footer"><a href="">View all</a></div>
+                    </div>
+                  </li>
+                  <li className="mr-4 text-xl relative"><IoMdNotifications/>
+                    <div className="dropdown">
+                      <div className="dropdown-menu-header notifications">Notifications</div>
+                      <ul className="dropdown-menu">
+                        <li className="items"></li>
+                        <li className="items"></li>
+                        <li className="items"></li>
+                        <li className="items"></li>
+                        <li className="items"></li>
+                      </ul>
+                      <div className="dropdown-menu-footer"><a href="">View all</a></div>
+                    </div>
+                  </li>
+                  <li className="mr-4 flex items-center md:flex-no-wrap"><div className="rounded-2xl w-6 h-6 bg-slate-500"><img className="" src="" alt=""/></div>
                   Hello!<span className="font-bold"> Ndubuisi</span></li>
                 </ul>
                 <button type="button"  className="md:hidden btn p-4 focus:outline-none hover:bg-gray-800" onClick={()=>toggleMenu()}>
--- src/Components/SideMenu/MenuUserProfile.tsx
+++ src/Components/SideMenu/MenuUserProfile.tsx
@@ -6,7 +6,7 @@ export default function MenuUserProfile() {
     <div className="flex items-center space-x-2">
         {/* <!-- AVATAR IMAGE BY FIRST LETTER OF NAME --> */}
         <img src="https://ui-avatars.com/api/?name=Habib+Mhamadi&size=128&background=ff4433&color=fff" className="w-7 rounded-full" alt="Profile"/>
-        <h1>Habib Mhamadi</h1>
+        <h1>Ndubuisi O.</h1>
     </div>
     <a  href="#" className="hover:bg-gray-800 hover:text-white p-2 rounded">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"></path></svg>       
